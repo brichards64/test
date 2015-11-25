346a11ef7d46debd62c1996075ee2c0acad5f8c6
brichards <b.richards@qmul.ac.uk>
2015-11-25 14:44:07 +0000

    et

--- build.sh
+++ build.sh
@@ -8,10 +8,8 @@ cd brichards64.github.io/
 
 echo test2 > index.html
 
- eval "$(ssh-agent -s)" #start the ssh agent
- chmod 600 .travis/deploy_key.pem # this key should have push access
- ssh-add .travis/deploy_key.pem
+ cd "$TRAVIS_BUILD_DIR"
 
 git commit -a -m test
 
-git push
+ git push -f -q https://brichards64:$GITHUB_API_KEY@github.com/brichards64/brichards64.github.io  &2>/dev/null
