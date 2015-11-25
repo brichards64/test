a708e25540359d012a5e3f8ff286142bb467fab9
brichards <b.richards@qmul.ac.uk>
2015-11-25 22:31:08 +0000

    test

--- build.sh
+++ build.sh
@@ -10,6 +10,10 @@ echo test2 > index.html
 
  cd "$TRAVIS_BUILD_DIR"
 
+ git config --global user.email "b.richards@qmul.ac.uk.com"
+  git config --global user.name "brichards64"
+
 git commit -a -m test
 
  git push -f -q https://brichards64:$GITHUB_API_KEY@github.com/brichards64/brichards64.github.io.git   &2>/dev/null
+
