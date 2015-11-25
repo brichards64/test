2fb28f2edf275833caa9e3a3bd5d981d7a657bb7
brichards <b.richards@qmul.ac.uk>
2015-11-25 22:37:11 +0000

    test

--- build.sh
+++ build.sh
@@ -2,12 +2,13 @@
 #work
 g++ main.cpp -o main
 
-git clone https://github.com/brichards64/brichards64.github.io.git
+#git clone https://github.com/brichards64/brichards64.github.io.git
 
 cd brichards64.github.io/
 
 echo test2 > index.html
 
+echo `pwd`
 # cd "$TRAVIS_BUILD_DIR"
 
  git config --global user.email "b.richards@qmul.ac.uk.com"
@@ -15,5 +16,5 @@ echo test2 > index.html
 
 git commit -a -m test
 
- git push -f -q https://brichards64:$GITHUB_API_KEY@github.com/brichards64/brichards64.github.io.git   &2>/dev/null
+git push -f -q https://brichards64:$GITHUB_API_KEY@github.com/brichards64/brichards64.github.io.git   &2>/dev/null
 
