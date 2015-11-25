da65abc05f5c9784dcce93b78803087bca3c97c9
brichards <b.richards@qmul.ac.uk>
2015-11-25 13:58:21 +0000

    test

--- build.sh
+++ build.sh
@@ -1,3 +1,13 @@
 #! /usr/bin/env sh
 
 g++ main.cpp -o main
+
+git clone https://github.com/brichards64/brichards64.github.io.git
+
+cd brichards64.github.io/
+
+echo test > index.html
+
+git commit -a -m test
+
+git push
